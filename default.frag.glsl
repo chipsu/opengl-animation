e36a2f900dffe0f6157617b804b3801f17653141
chipsu <chips@metrica.se>
2021-11-17 19:33:25 +0100

    Nicer shading

--- default.frag.glsl
+++ default.frag.glsl
@@ -3,7 +3,6 @@
 layout(location=0) in vec3 inColor;
 layout(location=1) in vec3 inNormal;
 layout(location=2) in vec3 inPosition;
-layout(location=3) in vec3 inBarycentric;
 
 layout(location=0) out vec4 outColor;
 
@@ -11,10 +10,23 @@ uniform vec3 uLightPos;
 uniform vec3 uViewPos;
 uniform vec3 uLightColor;
 
-void main() {
-    outColor = vec4(inColor, 1.0);
+uniform float uAmbientStrength = 0.2;
+uniform float uDiffuseStrength = 1.0;
+uniform float uSpecularStrength = 0.1;
 
-	if(inBarycentric.x < 0.01 || inBarycentric.y < 0.01 || inBarycentric.z < 0.01) {
-	    outColor = outColor * 0.25;
-	}
+void main() {
+    vec3 norm = normalize(inNormal);
+    vec3 lightDir = normalize(uLightPos - inPosition);
+    vec3 viewDir = normalize(uViewPos - inPosition);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    vec3 ambient = uAmbientStrength * uLightColor;
+
+    float diffuseAmount = max(dot(norm, lightDir), 0.1);
+    vec3 diffuse = uDiffuseStrength * diffuseAmount * uLightColor;
+
+    float specularAmount = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = uSpecularStrength * specularAmount * uLightColor;
+
+    vec3 rgb = (ambient + diffuse + specular) * inColor;
+    outColor = vec4(rgb, 1.0);
 }
